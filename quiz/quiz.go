e049a5808303fc0c18469b73b9dc84f9ed6a704f
Thiago Cangussu <tscangussu@protonmail.com>
2019-04-10 18:21:17 -0300

    Added a timer to the quiz with goroutine and channels

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
 type problem struct {
@@ -14,6 +15,7 @@ type problem struct {
 
 func main() {
 	csvFile := flag.String("csv", "problems.csv", "a CSV file to be parsed in a format 'question,answer'")
+	timeLimit := flag.Int("limit", 10, "time limit for the quiz in seconds")
 	flag.Parse()
 
 	file, err := os.Open(*csvFile)
@@ -34,6 +36,9 @@ func main() {
 
 	problems := make([]problem, len(questions))
 
+	// Here we put the slice returned by ReadAll() into a struct. Why? If instead
+	// of a CSV file we had a JSON, YAML or something, we only need to change the
+	// parser code, the rest remains the same.
 	for i, p := range questions {
 		problems[i] = problem{
 			q: p[0],
@@ -43,15 +48,38 @@ func main() {
 
 	var correct int
 	var answer string
+	aChannel := make(chan string)
 
+	// NewTimer() creates a new type Timer that will send the current time on its
+	// channel after the duration
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+
+	// The problem here is you want to ask the user a question while the timer is
+	// running but you don't want to accept any answer after the timer has
+	// expired. Since NewTimer() returns the time through a channel, you can use
+	// select and break the loop when the timer is expired. Select waits on
+	// multiple channel operations so you have to put the code that receives the
+	// user answer on a goroutine, so if the timer is expired select breaks the
+	// loop instead of accept the answer.
+loop:
 	for i, p := range problems {
 		fmt.Printf("Problem %2d : %s = ", i+1, p.q)
-		fmt.Scanf("%s\n", &answer)
 
-		if answer == p.a {
-			correct++
+		go func() {
+			fmt.Scanf("%s\n", &answer)
+			aChannel <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Println()
+			break loop
+		case answer := <-aChannel:
+			if answer == p.a {
+				correct++
+			}
 		}
-	}
 
-	fmt.Printf("You scored %d", correct)
+	}
+	fmt.Printf("\nYou scored %d from %d", correct, len(problems))
 }
