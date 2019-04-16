eff7da59fa94a0877e7f510d20ef6ff5bfcc7873
Thiago Cangussu <tscangussu@protonmail.com>
2019-04-15 21:29:33 -0300

    Added handler to render HTML template with data provided by a JSON file

--- cyoa/cyoa.go
+++ cyoa/cyoa.go
@@ -1,19 +1,105 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"text/template"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "It Works! \n %s", r.URL.Path[1:])
+type story map[string]chapter
+
+type chapter struct {
+	Title   string   `json:"title"`
+	Story   []string `json:"story"`
+	Options []struct {
+		Text string `json:"text"`
+		Arc  string `json:"arc"`
+	} `json:"options"`
+}
+
+var tmpl = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>{{.Title}}</title>
+  </head>
+	<body>
+		<main>
+			<section>
+				<header>
+					<h1>{{.Title}}</h1>
+				</header>
+				<article>
+					{{range .Story}}
+						<p>{{.}}</p>
+					{{end}}
+					<hr>
+					<ul>
+					{{range .Options}}
+					<li>
+						<a href="/{{.Arc}}">{{.Text}}</a>
+					</li>
+					{{end}}
+				</article>
+			</section>
+		</main>
+  </body>
+</html>
+`
+
+func handler(j []byte) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path[1:]
+		if len(path) < 1 {
+			path = "intro"
+		}
+
+		chapter := &path
+
+		p, err := parseJSON(j)
+		if err != nil {
+			log.Fatalf("JSON couldn't be parsed %v", err)
+		}
+
+		t, err := template.New("tmpl").Parse(tmpl)
+		if err != nil {
+			log.Fatalf("Template couldn't be parsed %v", err)
+		}
+
+		err = t.Execute(w, p[*chapter])
+		if err != nil {
+			log.Fatalf("Template execution failed %v", err)
+		}
+
+	}
+}
+
+func parseJSON(j []byte) (story, error) {
+	var ret story
+
+	if err := json.Unmarshal(j, &ret); err != nil {
+		return nil, err
+	}
+
+	return ret, nil
 }
 
 func main() {
+	jsonFile, err := ioutil.ReadFile("gopher.json")
+
+	if err != nil {
+		log.Fatalf("Failed to open JSON file \n %v", err)
+	}
+
+	h := handler(jsonFile)
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", handler)
+	mux.HandleFunc("/", h)
 
 	s := &http.Server{
 		Addr:    ":8080",
